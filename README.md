27154f01b1482a8b767fdc63c9cecafb1d801a6d
HR <hannahroksanne@gmail.com>
2024-09-01 00:05:54 -0700

    progress

--- README.md
+++ README.md
@@ -3,136 +3,58 @@
 
 ## data super store [WIP]
 
-datass helps you finish quicker.
-
-its lightweight, flexible, and unopinionated. if ur feeling like switching it up, you should give datass a shot.
-
-## datass is easy
-
-(honestly i havent tried it, myself, but i heard datass is easy af. my homie said that anybody could pick up datass with just a few one liners, and he donesn't know _anybody_ that has tried datass and not fallen in love.)
+its lightweight, flexible, and unopinionated. if ur feeling like switching it up, u should give datass a shot.
 
 ```ts
 import { datass } from 'datass'
 
-const canLie = datass(false)
-const usersLikes = datass(['big butts', 'honesty'])
-const userName = datass('sir mixalot')
+const $isReady = datass.boolean(false)
+$isReady.set(true)
+$isReady.toggle()
+$isReady.use()
+$isReady.state
+
+const isNotReady = $isReady.use((isReady) => !isReady)
 ```
-
-datass doesn't care if the data you fill it with is real or fake, or even if you has to use some third party data enhancement tools, and it doesnt give a damn about any context -- it isn't intersted in your "features" or whatever.
-
-datass is only interested in **one** thing. it just wants to get the job done.
 
 ```ts
-const brothersIncapableOfDenying = datass(['richard', 'harambe', 'hank "the tank" mardukas'])
+const $count = datass.number(0)
+$count.set(100)
+$count.increment()
+$count.decrement()
+$count.use()
+$count.state
 
-const userStats = datass({
-	age: 69,
-	height: 6.9,
-	weight: 420
-})
+const isCountEven = $count.use((count) => count % 2 === 0)
+$count.set(102) // ^ this hook will not cause a re-render
+// because the result of the selector using count as 102 is
+// the same result as using count as 100.
 ```
-
-## datass is safe
-
-quit worryyyyying. you dont need protection - datass is a safe alternative to the tradtional tooling.
-
-datass doesn't have a lot of rules and will allow you to do pretty much whatever you want while it adjusts to your needs. datass doesn't throw errors all the time like ur previous flings, and it may even be ok if it is not the only thing you're putting your data in.
-
-datass has a lot of people pleasing tendencies, honestly. it truely doesn't want to throw off the vibe -- but if you do try to stick something in datass without properly preparing it, it might let out get a little mouthy.
 
 ```ts
-const permittedPenetrationTester = datass<string>('dave')
+const $name = datass.string('world')
+$name.set('datass')
+$name.use()
+$name.state
 
-// other testers to permit
-const proposedPenetrationTesters = ['dave', 'rodney', 'greg', 'katherine']
-
-permittedPenetrationTester.set(otherPenetrationTesters)
-// datass: "the last we spoke, we agreed it would only be you, dave..."
+const firstLetter = $name.use((name) => name[0])
+$name.set('debbie') // ^ this hook will not cause a re-render
+// because the result of the selector using 'debbie' as the
+// name is the same result as using 'datass' as the name.
 ```
-
-**NOTE:** generally, you should not be trying to coerce datass into accepting different types of things that it is not ready for, and you find other ways to reason about what you decide to put in datass and what you expect to get out of it. always give datass heads up before you just cram something random in there.
-
-## datass doesnt care about size
-
-whether your data is just a wee little string or a gargantuan object, datass is ready to take it on.
 
 ```ts
-const weeLittleString = datass<string>('weeee')
-
-const gargantuanObject = datass({
-	name: 'gargantuan leviathan',
-	nickname: 'levi the G',
-
-	description:
-		'The gargantuan leviathan possesses a near limitless amount of stamina and the creature is never really shown getting tired or exhausted after swimming for long durations of time.',
-
-	funFact: 'Its top speed is around 80 miles per hour.'
-})
+const $users = datass.array(['alice', 'bob', 'charlie'])
+$users.set(['alice', 'bob', 'charlie', 'daniel'])
+$users.useMap((user) => user.length)
+$users.useFilter((user) => user.length > 5)
+$users.state
 ```
 
-## use datass
-
-```tsx
-import { datass } from 'datass'
-
-type UserT = {
-  name: string
-  likes: string[]
-}
-
-const $newSongLyrics = datass<string>('i like datass and i cannot lie')
-
-const $user = datass({
-  name: 'sir mixalot',
-  canLie: false,
-  likes: ['big butts', 'honesty']
-})
-
-const ComponentA = () => {
-  const user = $user.use()
-
-  return (
-    <div>
-      <input value={$user} onChange={$user.set}>
-    </div>
-  )
-}
+```ts
+const $person = datass.object({ name: 'alice', age: 20 })
+$person.use((person) => person.name)
+$person.set({ name: 'bob', age: 25 }) // causes a re-render
+$person.set({ name: 'bob', age: 99 }) // does not cause a re-render
+$person.state
 ```
-
-```tsx
-const ComponentB = () => {
-	// if state is an object, you can access it by key or
-	// by a path to access a piece of data within the object.
-	const usersFavoriteThing = $user.use('likes[0]')
-	const userCanLie = $user.use('canLie')
-	// or you can use the mighty selector, in which case
-	// state only updates when it no longer evaluates to "sir"
-	const userFirstName = $user.use((user) => user.name.split(' ')[0])
-	// or you can just get the whole state
-	const user = $user.use()
-	// or you can not subscribe at all and just access the
-	// state's values...
-	const userLikes = $user.state.likes
-	const userCanLie = $user.state.canLie
-
-	// or maybe youre not trying to access data, you're trying
-	// to update it. silly me, here your sign:
-	$user.set({
-		name: 'sir liesalot',
-		canLie: true,
-		likes: ['big butts', 'honesty', 'lying']
-	})
-
-	$someStringState.set('datass is the best')
-	$someStringState.set(event) // will auto handle event.target.value
-	$whateverState.set((state) => state + 1)
-	$someObjectState.set((state) => ({ newKey: 'newValue' }))
-	$someArrayState.set((state) => [...state, 'newItem'])
-	$someState.reset()
-}
-```
-
-## datass will answer the call
-
-day or night, if you've got data and you need something to react to it in a way that gives you confidence that you know what the fuck youre doing -- datass is the one for you.
--- src/datass.ts
+++ src/datass.ts
@@ -1,103 +1,260 @@
-import { useState, useEffect, useMemo, useCallback } from 'react'
+import { useState, useEffect, useId } from 'react'
+import safeGet from 'just-safe-get'
 
-const hq = {
-	// When datass(value) is called, a new id is generated
-	// and the store for the value is associated with said id.
-	associations: new Map()
-}
-
-const createAssociation = (store) => {
-	hq.associations[store.id] = store
-}
-
-const createStateSetter = (store) => {
-	const history = [store.state]
-
-	function setState(value) {
-		if (Object.is(store.state, value)) return
-		history.push(value)
-		store.state = value
-		store.subscribers.forEach((subscriber) => subscriber(store.state))
-	}
-
-	setState.history = history
-	return setState
-}
-
-type AssStore<T> = {
-	state: T
+type SubscriberT = {
 	id: string
-	subscribe: (listener) => void
-	subscribers: Set<(state: T) => void>
-	setState: (value: T) => void
+	update: (newState: any) => void
+	derive?: (newState: any) => any[]
+	previousValue: any
 }
 
-const createInitialStore = <T>(initialState: T) => {
-	const subscribers = new Set()
-	const state = structuredClone<T>(initialState)
-	const id = Math.random().toString(36).slice(2)
-	const store = { state, subscribers, id } as AssStore<T>
-	store.setState = createStateSetter(store)
-	store.subscribe = createSubscription(store)
-	hq.associations.set(id, store)
-	return store
+const datMainAss = {
+	stores: new Map()
 }
 
-const createSubscription = (store) => {
-	return (listener) => {
-		store.subscribers.add(listener)
-		return () => store.subscribers.delete(listener)
-	}
-}
-
-// type StoreListenerT<T> = (state: T) => void
-
-function createStore<T>(initialState: T) {
-	const store = createInitialStore<T>(initialState)
-	createAssociation(store)
-	return store
-}
-
-function _useStore<T, X = T>(store: ReturnType<typeof createStore<T>>, selector?: (value: T) => X) {
-	const [state, setState] = useState(() => (selector ? selector(store.state) : store.state))
-
-	const memoizedSelector = useCallback(selector || ((x: T) => x as unknown as X), [selector])
-
-	const memoizedSetState = useCallback(
-		(newState: T) => {
-			setState((prevState) => {
-				const nextState = memoizedSelector(newState)
-				return Object.is(prevState, nextState) ? prevState : nextState
-			})
-		},
-		[memoizedSelector]
-	)
-
-	useEffect(() => {
-		const unsubscribe = store.subscribe(memoizedSetState)
-		return unsubscribe
-	}, [store, memoizedSetState])
-
-	return useMemo(
-		() => ({
-			get state() {
-				return state
-			}
-		}),
-		[state]
-	)
-}
-
-export function datass<T>(initialState: T) {
-	const store = createStore(initialState)
-
-	return {
-		__datassIs: store.id,
-		set: (value: T) => store.setState(value),
-		use: (selector?: <X>(value: T) => X) => _useStore(store, selector),
+function createInternals<T>(initialState: T) {
+	const internals: InternalsT<T> = {
+		id: datMainAss.stores.size,
+		currentState: initialState,
+		previousState: null,
+		subscribers: new Map(),
 
 		get state() {
-			return store.state
+			return internals.currentState
+		},
+
+		use(selector?: (state: T) => any) {
+			const id = useId()
+			const initialValue = selector ? selector(internals.state) : internals.state
+			const [value, setValue] = useState(initialValue)
+
+			useEffect(() => {
+				return internals.subscribe({
+					derive: selector,
+					previousValue: value,
+					update: setValue,
+					id
+				})
+			}, [])
+
+			return value
+		},
+
+		unsubscribe(id: string) {
+			// console.log('[datass] unsubscribe', id)
+			internals.subscribers.delete(id)
+		},
+
+		subscribe(subscriber: SubscriberT) {
+			internals.subscribers.set(subscriber.id, subscriber)
+			return () => internals.unsubscribe(subscriber.id)
+		},
+
+		replaceState(newState: T) {
+			internals.previousState = internals.currentState
+			internals.currentState = newState
+
+			internals.subscribers.forEach((subscriber) => {
+				if (subscriber.derive) {
+					const newResult = subscriber.derive(newState)
+					const areEqual = areArrayItemsEqual(subscriber.previousValue, newResult)
+
+					if (!areEqual) {
+						subscriber.update(newResult)
+						subscriber.previousValue = newResult
+					}
+
+					return
+				}
+
+				if (subscriber.previousValue !== newState) {
+					subscriber.update(newState)
+					subscriber.previousValue = newState
+				}
+			})
 		}
 	}
+
+	datMainAss.stores.set(internals.id, internals)
+	return internals
+}
+
+type InternalsT<T> = {
+	id: number
+	currentState: T
+	previousState: T | null
+	subscribers: Map<string, SubscriberT>
+	get state(): T
+	use(): T
+	replaceState: (newState: T) => void
+	subscribe: (subscriber: SubscriberT) => () => void
+	unsubscribe: (id: string) => void
+}
+
+const datBoolean = (initialState: boolean) => {
+	const internals = createInternals(initialState)
+
+	const toggle = (value?: boolean) => {
+		internals.replaceState(value ?? !internals.state)
+	}
+
+	return getFinalStore({ internals, toggle })
+}
+
+type GetFinalStoreOptionsT = {
+	[key: string]: any
+}
+
+const getFinalStore = (options: GetFinalStoreOptionsT) => {
+	const { internals, ...rest } = options
+
+	const identify = (name: string) => {
+		const datassAssociation = datMainAss.stores.get(internals.id)
+		datMainAss.stores.delete(internals.id)
+		datMainAss.stores.set(name, datassAssociation)
+	}
+
+	return {
+		identify,
+		use: internals.use,
+		set: internals.replaceState,
+		...rest,
+		get state() {
+			return internals.state
+		}
+	}
+}
+
+const datNumber = (initialState: number) => {
+	const internals = createInternals(initialState)
+
+	const increment = () => {
+		internals.replaceState(internals.state + 1)
+	}
+
+	const decrement = () => {
+		internals.replaceState(internals.state - 1)
+	}
+
+	return getFinalStore({ internals, increment, decrement })
+}
+
+const datString = (initialState: string) => {
+	const internals = createInternals(initialState)
+	return getFinalStore({ internals })
+}
+
+const datArray = <T>(initialState: T[]) => {
+	const internals = createInternals<T[]>(initialState)
+
+	const useFilter = (filterCallback: (item: T) => boolean) => {
+		const id = useId()
+		const initialResult = internals.state.filter(filterCallback)
+		const [value, setValue] = useState(initialResult)
+
+		// subscribe to the ITEMS that are returned in the array
+		// from calling internals.state.filter(filterCallback)
+		// it needs to subscribe with its id, most up to date results,
+		// and its function to determine new results / compare old results
+
+		useEffect(() => {
+			const derive = (newState: T[]): any[] => {
+				return newState.filter(filterCallback)
+			}
+
+			return internals.subscribe({
+				previousValue: value,
+				update: setValue,
+				derive,
+				id
+			})
+		}, [])
+
+		return value
+	}
+
+	const useMap = (mapCallback: (item: T) => any) => {
+		const id = useId()
+		const initialResult = internals.state.map(mapCallback)
+		const [value, setValue] = useState(initialResult)
+
+		useEffect(() => {
+			const derive = (newState: T[]): any[] => {
+				return newState.map(mapCallback)
+			}
+
+			return internals.subscribe({
+				id,
+				previousValue: value,
+				update: setValue,
+				derive
+			})
+		}, [])
+
+		return value
+	}
+
+	return getFinalStore({ internals, useFilter, useMap })
+}
+
+// Helper function to compare array items
+const areArrayItemsEqual = (arr1: any[], arr2: any[]): boolean => {
+	if (arr1.length !== arr2.length) return false
+
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) return false
+	}
+
+	return true
+}
+
+const datObject = <T>(initialState: T) => {
+	const internals = createInternals<T>(initialState)
+
+	const usePartial = (path: string) => {
+		const id = useId()
+		const initialResult = safeGet(internals.state as any, path)
+		const [value, setValue] = useState(initialResult)
+
+		useEffect(() => {
+			const derive = (newState: T[]): any[] => {
+				return safeGet(newState, path)
+			}
+
+			return internals.subscribe({
+				id,
+				previousValue: value,
+				update: setValue,
+				derive
+			})
+		}, [])
+
+		return value
+	}
+
+	const finalStore = getFinalStore({ internals, usePartial })
+	return finalStore as unknown as DatassStoreT<T> & DatassObjectStoreT
+}
+
+type DatassStoreT<T> = {
+	use: {
+		(): T
+		<T>(selector: (state: T) => any): any
+	}
+
+	set: (newState: T) => void
+	identify: (name: string) => void
+	state: T
+}
+
+type DatassObjectStoreT = {
+	usePartial: <T>(path: string) => T
+}
+
+export const datass = {
+	boolean: datBoolean,
+	number: datNumber,
+	string: datString,
+	array: datArray,
+	object: datObject
 }
